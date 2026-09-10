965579d965f2731103067981b10105f0deea152a
Souvick Kumar Halder <reach.souvick@gmail.com>
2026-09-11 02:16:50 +0530

    Add

--- src/data/tournament.js
+++ src/data/tournament.js
@@ -1,7 +1,7 @@
 export const tournamentInfo = {
-  name: 'Gulabi Devi Memorial Cup', edition: '14th Edition', year: '2026', organizer: 'Budge Budge Institute of Technology', sponsor: 'JIMSH Hospital', status: 'REGISTRATION OPENS SOON', date: null, registrationDeadline: null,
+  name: 'Gulabi Devi Memorial Cup', edition: '14th Edition', year: '2026', organizer: 'Budge Budge Institute of Technology', sponsor: 'JIMSH Hospital', status: 'DATES CONFIRMED', date: '25–28 Sep 2026', registrationDeadline: null,
   venue: 'BBIT Football Ground', address: 'Budge Budge Institute of Technology, Nischintapur, Budge Budge, Kolkata, West Bengal', mapLink: 'https://share.google/cbsoChs9leBAwYUwF', entryFee: 3000, format: 'League-cum-Knockout', matchType: '11-a-side Football', teams: 0,
-  contacts: [{ name: 'Sumit', phone: '9609662550' }, { name: 'Rajen', phone: '9641461347' }, { name: 'Susovon', phone: '9832912773' }, { name: 'Deba', phone: '9547069495' }], socialLinks: { instagram: '', facebook: '', youtube: '' }
+  contacts: [{ name: 'Sumit', phone: '9609662550' }, { name: 'Deba', phone: '9547069495' }], socialLinks: { instagram: '', facebook: '', youtube: '' }
 };
 export const teams = [];
 export const fixtures = [];
